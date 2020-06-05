da13c1f5097c5b3da4613552cff0d60d04f6f02a
monika-sahay <monika_sahay@yahoo.com>
2020-06-05 18:57:57 +0100

    addded amange user endponit

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+
 from user.serializers import UserSerializer, AuthTokenSerializer
 
 
@@ -13,3 +14,16 @@ class CreateTokenView(ObtainAuthToken):
     '''create a new auth token for user'''
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    '''manage the authenticated user'''
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (
+        permissions.IsAuthenticated,
+        )
+
+    def get_object(self):
+        '''Retrieve and return authenticated users'''
+        return self.request.user
