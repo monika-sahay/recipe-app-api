dae7ae26bc9453ce15e70dc59edef3b966f4faac
monika-sahay <monika_sahay@yahoo.com>
2020-06-08 17:46:40 +0100

    added list recipe featur

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -51,3 +51,15 @@ class IngredientViewSet(BaseRecipeAttrrViewSet):
 #    def perform_create(self, serializer):
 #        '''Create a new ingredient'''
 #        serializer.save(user=self.request.user)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    ''' manage recipes in the database'''
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        ''' Retrieve the recipes for the authenticated users'''
+        return self.queryset.filter(user=self.request.user)
